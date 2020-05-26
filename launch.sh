9babcfde73a46eb841181cad423dbff19ed25a08
bob chase <bob.chase@gmail.com>
2020-05-26 07:04:11 -0400

    cleanup

--- launch.sh
+++ launch.sh
@@ -1,8 +1,6 @@
-DOCKER=${GIT:-$HOME/git}/docker-neovim
-
 function nv {
     local launch_dir=$(pwd)
-    pushd $DOCKER > /dev/null
+    pushd ${DOCKER_NEOVIM:-${GIT:-$HOME/git}/docker-neovim} > /dev/null
     LAUNCH_DIR="$launch_dir" sh neovim.sh "$*"
     popd > /dev/null
 }
