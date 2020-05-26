c968082dda5fd041b269f1c1c1e4eb77f41c20d9
bob chase <bob.chase@gmail.com>
2020-05-26 10:53:50 -0400

    clearer env var naming

--- function.sh
+++ function.sh
@@ -1,6 +1,6 @@
 function nv {
     local launch_dir=$(pwd)
-    pushd ${DOCKER_NEOVIM:-${GIT:-$HOME/git}/docker-neovim} > /dev/null
+    pushd ${DOCKER_NEOVIM_DIR:-${GIT:-$HOME/git}/docker-neovim} > /dev/null
     LAUNCH_DIR="$launch_dir" sh neovim.sh "$*"
     popd > /dev/null
 }
